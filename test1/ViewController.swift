a0927ac7b630f45fb7571188fc5c9760aadb0c21
Madhuranga Senadheera <madhuranga.senadheera@softlogic.lk>
2020-02-05 12:47:17 +0530

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Madhuranga Senadheera on 2/5/20.
+//  Copyright © 2020 Madhuranga Senadheera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
